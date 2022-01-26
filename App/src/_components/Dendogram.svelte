ef0b5bddcf99e54c60d77623932318edde0197a3
JannesPeeters <jannes.peeters@live.com>
2022-01-26 15:03:46 +0100

    parameter change for pathGenerator function

--- App/src/_components/Dendogram.svelte
+++ App/src/_components/Dendogram.svelte
@@ -1,6 +1,8 @@
 <script>
-
+    
     import { pathGenerator } from '../_js/functions.js';
+    import { max } from 'd3-array';
+    import { scaleLinear } from 'd3-scale';
 
     export let data = [];
     export let n;
@@ -9,17 +11,22 @@
     const width = 500; 
     const height = 200;
 
+    const xScale = scaleLinear().domain([0, n]).range([0, width]);
+    const yScale = scaleLinear().domain([max(data.nodes, node => node["depth"]), 0]).range([0, height]).nice();
+
     let svg, g_dendogram;
 </script>
 
 
-<svg width="100%" viewBox={`0 0 ${width} ${height}`} bind:this={svg}>
+
+<svg viewBox={`0 0 ${width} ${height}`} bind:this={svg}>
     <g transform={`translate(${bandwidth/2},0)`} bind:this={g_dendogram}>
-        {#each data.links as clust_link}
-            <path d={pathGenerator(clust_link, data.nodes, n, width, height)}
+        {#each data.links as link}
+            <path d={pathGenerator(link, xScale, yScale)}
                 fill="none"
                 stroke="grey">
             </path>
         {/each}
+        <!-- <line x1={0} x2={width} y1={} y2={} stroke="black"></line> -->
     </g>
 </svg>
