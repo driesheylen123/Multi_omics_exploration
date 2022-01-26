0714202c01fd650b47333319aa730be00255bca9
JannesPeeters <jannes.peeters@live.com>
2022-01-26 13:58:30 +0100

    moved Dendogram.svelte into Heatmap.svelte

--- App/src/App.svelte
+++ App/src/App.svelte
@@ -3,9 +3,8 @@
 	import Sidebar from './_components/Sidebar.svelte';
 	import Heatmap from './_components/Heatmap.svelte';
 	import Network from './_components/Network.svelte';
-	import Dendogram from './_components/Dendogram.svelte';
 
-	import { _data, renderVisuals, linkage } from './stores.js';
+	import { _data, renderVisuals } from './stores.js';
 		
 </script>
 
@@ -13,14 +12,7 @@
 	<Sidebar></Sidebar>
 	{#if $renderVisuals}
 		<div class="flex-grow-1 align-self-center mx-3 mh-100" id="heatmap">
-			{#if $linkage !== "none"}
-				<div>
-					<Dendogram data={$_data}></Dendogram>
-				</div>
-			{/if}
-			<div>
-				<Heatmap data={$_data}></Heatmap>
-			</div>
+			<Heatmap data={$_data}></Heatmap>
 		</div>
 		<div class="flex-grow-1 align-self-center mx-3 mh-100" id="network">
 			<Network data={$_data}></Network>
